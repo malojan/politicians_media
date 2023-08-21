f299a64e674c3f0ee2fe9f6601e14cfdb5aff06b
Malo Jan <malo.jan@sciencespo.fr>
2023-08-21 23:46:33 +0200

    Update the documentation and Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Collect data on french politicians' media appearances
+
+This repo contains a script that collect data on the media appearances of french politicians, scraping information from [politiquemedia](https://www.politiquemedia.com/). The webiste monitors and records the invitations of politicians on TV and radio broadcasts. 
--- polmedia_scraping.R
+++ polmedia_scraping.R
@@ -1,19 +1,25 @@
 
 
-# Import packages
+# Importing the required libraries
+# `tidyverse` provides a collection of R packages for data science.
+# `rvest` helps in web scraping tasks.
 
 needs(tidyverse, rvest)
 
-# Get url for the page of each politician
+# The following code block is aimed at retrieving URLs for individual politician pages
+# from the main directory of politicians.
 
+# Starting with the main directory page
 pages_politicians <-
-  read_html("https://www.politiquemedia.com/personnalites.html") |>
-  html_nodes(".personnalites-nom > a") |>
+  read_html("https://www.politiquemedia.com/personnalites.html") %>%
+  # Extracting the anchor links for each politician
+  html_nodes(".personnalites-nom > a") %>%
+  # Fetching the 'href' attribute which has the URL
   html_attr("href") %>%
+  # Prepending the base URL to each extracted link
   str_c("https://www.politiquemedia.com/", .)
 
-# Get content of each page
-
+# Mapping French month names to numerical month representation for date conversion
 
 month_mapping <- c(
   " janvier " = "/01/",
@@ -30,11 +36,15 @@ month_mapping <- c(
   " décembre " = "/12/"
 )
 
-extract_content <- function(x) {
+# Function to extract content from each politician's individual page
 
+extract_content <- function(x) {
   page <- read_html(x, encoding = "utf-8")
 
-  get_text <- function(x) {html_element(page, x) |> html_text2()}
+  # Function that faciliate the text extraction from a given selector
+  get_text <- function(x) {
+    html_element(page, x) |> html_text2()
+  }
 
   media <- tibble(
     name = get_text(".nom-personnalite") |> str_replace("\n", " "),
@@ -45,11 +55,13 @@ extract_content <- function(x) {
     history = html_element(page, ".historique-complet") |> html_table()
   ) |>
     unnest(cols = history) |>
-    rename(date = X1,
-           time = X2,
-           channel = X4,
-           emission = X5) |>
-    select(- c(X3, X6)) |>
+    rename(
+      date = X1,
+      time = X2,
+      channel = X4,
+      emission = X5
+    ) |>
+    select(-c(X3, X6)) |>
     mutate(
       day = str_extract(date, ".+?(?=\\d+)"),
       date_parsed = str_remove(date |> str_squish(), ".+?(?=\\d+)") |> str_replace_all(month_mapping) |> dmy()
@@ -59,9 +71,15 @@ extract_content <- function(x) {
   return(media)
 }
 
-media_politicians <- map(pages_politicians[1:10], safely(extract_content), .progress = TRUE) |>
-  map('result') |>
-  compact() |>
+# Going through each politician's page to extract their media appearances
+
+media_politicians <-
+  map(pages_politicians, safely(extract_content), .progress = TRUE) %>%
+  # Mapping results and handling potential errors from `safely`
+  map('result') %>%
+  # Removing NULL results if any
+  compact() %>%
+  # Binding all individual results into a single data frame
   bind_rows()
 
-
+write_csv("polmedia.csv")
